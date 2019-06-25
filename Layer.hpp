afa54f22ce8e8585192688ba06ea143c2a53766a
Paul Nykiel <paul.nykiel@uni-ulm.de>
2019-06-25 15:18:30 +0200

    Added namespaces

--- Layer.hpp
+++ Layer.hpp
@@ -10,63 +10,66 @@
 #include <tuple>
 #include <random>
 
-template <int InputSize, int OutputSize>
-class Layer {
-public:
-    Layer() {
-        std::random_device rd;  //Will be used to obtain a seed for the random number engine
-        std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
-        std::normal_distribution<> dis;
-        for (auto &weight : weights) {
-            weight = dis(gen);
-        }
+namespace ml {
 
-        for (auto &bias : biases) {
-            bias = dis(gen);
-        }
-    }
-
-    auto propagate(const std::array<double, InputSize> &inputVec,
-                   const std::function<double(double)> &activationFunction) {
-        for(auto o = 0; o < OutputSize; o++) {
-            lastDendriticPotential[o] = 0;
-            for(auto i = 0; i < InputSize; i++) {
-                lastDendriticPotential[o] += inputVec[i] * weights[o * InputSize + i];
+    template<int InputSize, int OutputSize>
+    class Layer {
+    public:
+        Layer() {
+            std::random_device rd;  //Will be used to obtain a seed for the random number engine
+            std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+            std::normal_distribution<> dis;
+            for (auto &weight : weights) {
+                weight = dis(gen);
             }
-            lastDendriticPotential[o] += biases[o];
-            lastOutput[o] = activationFunction(lastDendriticPotential[o]);
-        }
-        return lastOutput;
-    }
 
-    auto backPropagate(const std::array<double, OutputSize> &errorVec,
-                       const std::function<double(double)> &transDiff) {
-        std::array<double, InputSize> errorInPrevLayer;
-        for(auto i = 0; i < InputSize; i++) {
-            errorInPrevLayer[i] = 0;
-            for(auto o = 0; o < OutputSize; o++) {
-                errorInPrevLayer[i] += errorVec[o] * weights[o * InputSize + i] * transDiff(lastDendriticPotential[o]);
+            for (auto &bias : biases) {
+                bias = dis(gen);
             }
         }
-        return errorInPrevLayer;
-    }
 
-    auto adaptWeights(const std::array<double, OutputSize> &errorVec,
-                      const std::array<double,InputSize> &input, double learnRate) {
-        for(auto o = 0; o < OutputSize; o++) {
-            for(auto i = 0; i < InputSize; i++) {
-                weights[o * InputSize + i] += errorVec[o] * learnRate * input[i];
+        auto propagate(const std::array<double, InputSize> &inputVec,
+                       const std::function<double(double)> &activationFunction) {
+            for (auto o = 0; o < OutputSize; o++) {
+                lastDendriticPotential[o] = 0;
+                for (auto i = 0; i < InputSize; i++) {
+                    lastDendriticPotential[o] += inputVec[i] * weights[o * InputSize + i];
+                }
+                lastDendriticPotential[o] += biases[o];
+                lastOutput[o] = activationFunction(lastDendriticPotential[o]);
             }
-            biases[o] += errorVec[o] * learnRate;
+            return lastOutput;
         }
-    }
 
-private:
-    std::array<double,InputSize*OutputSize> weights;
-    std::array<double, OutputSize> biases;
-    std::array<double, OutputSize> lastDendriticPotential;
-    std::array<double, OutputSize> lastOutput;
-};
+        auto backPropagate(const std::array<double, OutputSize> &errorVec,
+                           const std::function<double(double)> &transDiff) {
+            std::array<double, InputSize> errorInPrevLayer;
+            for (auto i = 0; i < InputSize; i++) {
+                errorInPrevLayer[i] = 0;
+                for (auto o = 0; o < OutputSize; o++) {
+                    errorInPrevLayer[i] +=
+                            errorVec[o] * weights[o * InputSize + i] * transDiff(lastDendriticPotential[o]);
+                }
+            }
+            return errorInPrevLayer;
+        }
 
+        auto adaptWeights(const std::array<double, OutputSize> &errorVec,
+                          const std::array<double, InputSize> &input, double learnRate) {
+            for (auto o = 0; o < OutputSize; o++) {
+                for (auto i = 0; i < InputSize; i++) {
+                    weights[o * InputSize + i] += errorVec[o] * learnRate * input[i];
+                }
+                biases[o] += errorVec[o] * learnRate;
+            }
+        }
+
+    private:
+        std::array<double, InputSize * OutputSize> weights;
+        std::array<double, OutputSize> biases;
+        std::array<double, OutputSize> lastDendriticPotential;
+        std::array<double, OutputSize> lastOutput;
+    };
+}
 
 #endif //MLPTEST_LAYER_HPP
--- Mlp.hpp
+++ Mlp.hpp
@@ -9,113 +9,118 @@
 #include <utility>
 #include "Layer.hpp"
 
-// Helper
-template<unsigned int index, unsigned int... remPack> struct getVal;
-template<unsigned int index, unsigned int In, unsigned int... remPack> struct getVal<index, In,remPack...>
-{
-    static const unsigned int val = getVal<index-1, remPack...>::val;
-};
-template<unsigned int In, unsigned int...remPack> struct getVal<1,In,remPack...>
-{
-    static const unsigned int val = In;
-};
-template<unsigned int...remPack> struct getLast
-{
-    static const unsigned int val = getVal<sizeof...(remPack), remPack...>::val;
-};
+namespace ml {
+    namespace helper {
+        template<unsigned int index, unsigned int... remPack>
+        struct getVal;
+        template<unsigned int index, unsigned int In, unsigned int... remPack>
+        struct getVal<index, In, remPack...> {
+            static const unsigned int val = getVal<index - 1, remPack...>::val;
+        };
+        template<unsigned int In, unsigned int...remPack>
+        struct getVal<1, In, remPack...> {
+            static const unsigned int val = In;
+        };
+        template<unsigned int...remPack>
+        struct getLast {
+            static const unsigned int val = getVal<sizeof...(remPack), remPack...>::val;
+        };
+    }
 
+    template<int INPUT, int OUTPUT, int ... FOLLOWING_NEURONS>
+    class Mlp {
+    public:
+        static constexpr auto LAST_OUTPUT = getLast<FOLLOWING_NEURONS...>::val;
+        using TransferF = std::function<double(double)>;
+        using CostF = std::function<double(std::array<double, LAST_OUTPUT>, std::array<double, LAST_OUTPUT>)>;
 
-template<int INPUT, int OUTPUT, int ... FOLLOWING_NEURONS>
-class Mlp {
-public:
-    static constexpr auto LAST_OUTPUT = getLast<FOLLOWING_NEURONS...>::val;
-    using TransferF = std::function<double(double)>;
-    using CostF = std::function<double(std::array<double, LAST_OUTPUT>, std::array<double, LAST_OUTPUT>)>;
-
-    Mlp(const TransferF &transfer, const TransferF &transferDiff, const CostF &costF,
+        Mlp(const TransferF &transfer, const TransferF &transferDiff, const CostF &costF,
             double learnRate)
-            :  followingMlp(transfer, transferDiff, costF, learnRate), layer{},
-                transfer(transfer), transferDiff(transferDiff), costF{costF}, learnRate(learnRate) {};
+                : followingMlp(transfer, transferDiff, costF, learnRate), layer{},
+                  transfer(transfer), transferDiff(transferDiff), costF{costF}, learnRate(learnRate) {};
 
-    auto forward(const std::array<double, INPUT> &x) -> std::array<double, LAST_OUTPUT> {
-        auto thisLayerResult = layer.propagate(x, transfer);
-        return followingMlp.forward(thisLayerResult);
-    }
-
-    auto train(const std::vector<std::array<double, INPUT>> &inputs,
-               const std::vector<std::array<double, LAST_OUTPUT>> &outputs, double maxError,
-               const std::optional<std::function<void(double)>> &errorCallback = std::nullopt) {
-        assert(inputs.size() == outputs.size());
-        auto error = std::numeric_limits<double>::max();
-        while (error > maxError) {
-            for (std::size_t c = 0; c < inputs.size(); c++) {
-                adapt(inputs[c], outputs[c]);
-            }
-
-            error = 0.0;
-            for (std::size_t c = 0; c < inputs.size(); c++) {
-                auto mlpOutput = forward(inputs[c]);
-
-                error += costF(mlpOutput, outputs[c]);
-            }
-
-            if (errorCallback.has_value()) {
-                errorCallback.value()(error);
-            }
+        auto forward(const std::array<double, INPUT> &x) -> std::array<double, LAST_OUTPUT> {
+            auto thisLayerResult = layer.propagate(x, transfer);
+            return followingMlp.forward(thisLayerResult);
         }
-        return error;
-    }
 
-    auto adapt(const std::array<double,INPUT> &input, const std::array<double, LAST_OUTPUT> &trainerOutput) -> std::array<double, INPUT> {
-        auto output = layer.propagate(input,transfer);
-        auto outputError = followingMlp.adapt(output, trainerOutput);
-        auto inputError = layer.backPropagate(outputError,transferDiff);
-        layer.adaptWeights(outputError, input, learnRate);
-        return inputError;
-    }
+        auto train(const std::vector<std::array<double, INPUT>> &inputs,
+                   const std::vector<std::array<double, LAST_OUTPUT>> &outputs, double maxError,
+                   const std::optional<std::function<void(double)>> &errorCallback = std::nullopt) {
+            assert(inputs.size() == outputs.size());
+            auto error = std::numeric_limits<double>::max();
+            while (error > maxError) {
+                for (std::size_t c = 0; c < inputs.size(); c++) {
+                    adapt(inputs[c], outputs[c]);
+                }
 
-private:
-    Mlp<OUTPUT, FOLLOWING_NEURONS...> followingMlp;
-    Layer<INPUT, OUTPUT> layer;
-    const TransferF transfer;
-    const TransferF transferDiff;
-    const CostF costF;
-    const double learnRate;
-};
+                error = 0.0;
+                for (std::size_t c = 0; c < inputs.size(); c++) {
+                    auto mlpOutput = forward(inputs[c]);
 
-template<int INPUT, int OUTPUT>
-class Mlp<INPUT,OUTPUT> {
-public:
-    static constexpr auto LAST_OUTPUT = OUTPUT;
-    using TransferF = std::function<double(double)>;
-    using CostF = std::function<double(std::array<double, LAST_OUTPUT>, std::array<double, LAST_OUTPUT>)>;
+                    error += costF(mlpOutput, outputs[c]);
+                }
 
-    Mlp(TransferF transfer, TransferF transferDiff, const CostF &costF,
+                if (errorCallback.has_value()) {
+                    errorCallback.value()(error);
+                }
+            }
+            return error;
+        }
+
+        auto adapt(const std::array<double, INPUT> &input,
+                   const std::array<double, LAST_OUTPUT> &trainerOutput) -> std::array<double, INPUT> {
+            auto output = layer.propagate(input, transfer);
+            auto outputError = followingMlp.adapt(output, trainerOutput);
+            auto inputError = layer.backPropagate(outputError, transferDiff);
+            layer.adaptWeights(outputError, input, learnRate);
+            return inputError;
+        }
+
+    private:
+        Mlp<OUTPUT, FOLLOWING_NEURONS...> followingMlp;
+        Layer <INPUT, OUTPUT> layer;
+        const TransferF transfer;
+        const TransferF transferDiff;
+        const CostF costF;
+        const double learnRate;
+    };
+
+    template<int INPUT, int OUTPUT>
+    class Mlp<INPUT, OUTPUT> {
+    public:
+        static constexpr auto LAST_OUTPUT = OUTPUT;
+        using TransferF = std::function<double(double)>;
+        using CostF = std::function<double(std::array<double, LAST_OUTPUT>, std::array<double, LAST_OUTPUT>)>;
+
+        Mlp(TransferF transfer, TransferF transferDiff, const CostF &costF,
             double learnRate)
-            :  layer{}, transfer(std::move(transfer)), transferDiff(std::move(transferDiff)), costF{costF}, learnRate(learnRate) {};
+                : layer{}, transfer(std::move(transfer)), transferDiff(std::move(transferDiff)), costF{costF},
+                  learnRate(learnRate) {};
 
-    std::array<double, OUTPUT> forward(std::array<double, INPUT> x) {
-        return layer.propagate(x, transfer);
-    }
-
-    auto adapt(std::array<double,INPUT> input, std::array<double, OUTPUT> trainerOutput) -> std::array<double, INPUT> {
-        auto mlpOutput = layer.propagate(input, transfer);
-        std::array<double, OUTPUT> outputErrror;
-        for(auto c=0; c<OUTPUT; c++) {
-            outputErrror[c] = trainerOutput[c] - mlpOutput[c];
+        std::array<double, OUTPUT> forward(std::array<double, INPUT> x) {
+            return layer.propagate(x, transfer);
         }
-        auto inputError = layer.backPropagate(outputErrror, transferDiff);
-        layer.adaptWeights(outputErrror, input, learnRate);
-        return inputError;
-    }
 
-private:
-    Layer<INPUT, OUTPUT> layer;
-    const TransferF transfer;
-    const TransferF transferDiff;
-    const CostF costF;
-    const double learnRate;
-};
+        auto
+        adapt(std::array<double, INPUT> input, std::array<double, OUTPUT> trainerOutput) -> std::array<double, INPUT> {
+            auto mlpOutput = layer.propagate(input, transfer);
+            std::array<double, OUTPUT> outputErrror;
+            for (auto c = 0; c < OUTPUT; c++) {
+                outputErrror[c] = trainerOutput[c] - mlpOutput[c];
+            }
+            auto inputError = layer.backPropagate(outputErrror, transferDiff);
+            layer.adaptWeights(outputErrror, input, learnRate);
+            return inputError;
+        }
 
+    private:
+        Layer <INPUT, OUTPUT> layer;
+        const TransferF transfer;
+        const TransferF transferDiff;
+        const CostF costF;
+        const double learnRate;
+    };
+}
 
 #endif //MLPTEST_MLP_HPP
